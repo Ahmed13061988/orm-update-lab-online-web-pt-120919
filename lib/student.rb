db22ef015adc96f84caaeae98f6a040d546b12f4
Ahmed Hussein <wolktheline@yahoo.com>
2020-02-27 22:26:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,12 +37,11 @@ class Student
     end
   end
   
-  def self.new_from_db(row)
-    id = row[0]
-    name = row[1]
-    grade = row[2]
-    self.new(id, name, grade)
-  end 
+  def self.create(name:, grade:)
+    student_new = self.new(name, grade)
+    student_new.save
+    student_new
+  end
   
   def self.new_from_db(row)
     id = row[0]
